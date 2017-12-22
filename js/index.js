ea2430eaab07df14cc860868c294467145bd3535
renx777 <arenukaprasad@gmail.com>
2017-12-23 02:44:12 +0530

    completed all user stories

--- js/index.js
+++ js/index.js
@@ -4,15 +4,16 @@ $.getJSON(
   function(x) {
     var margin = { top: 40, right: 20, bottom: 30, left: 40 },
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 530 - margin.top - margin.bottom;
 
     var svg = d3
-    .select("body")
+    .select("#graph")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
     var data = x;
     var rankings = [];
@@ -75,6 +76,15 @@ $.getJSON(
       .call(xAxis)
       .attr("transform", "translate(0," + height + ")");
 
+    // label for x axis
+    svg.append("text")    
+     .attr("class", "labels")
+      .attr("transform",
+            "translate(400," + 490 + ")")
+      .style("text-anchor", "middle")
+      .text("Minutes Behind Fastest Time");
+    
+    
     // draw yaxis
     svg
       .append("g")
@@ -85,6 +95,46 @@ $.getJSON(
       .attr("y", 6)
       .attr("dy", ".71em");
 
+//     label for y axis
+    
+   svg.append("text")
+    .attr("class", "labels")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Ranking");      
+// tooltip
+    
+    var tooltip = d3.select("body").append("div")
+                  .attr("class", "tooltip")
+                  .style("opacity", 0);
+
+              // tooltip mouseover event handler
+              var tipMouseover = function(data) {
+                  //var color = colorScale(d.manufacturer);
+                var color="black";
+                  var html  = "<span><b>"+data.Name + "</b></span><br/>" +
+                              "<span>" + data.Time+ "</span><br/>" +
+                              "<span>" + data.Doping + "</span> ";
+
+                  tooltip.html(html)
+                      .style("left", (d3.event.pageX + 15) + "px")
+                      .style("top", (d3.event.pageY - 28) + "px")
+                    .transition()
+                      .duration(200) // ms
+                      .style("opacity", .9) // started as 0!
+
+              };
+              // tooltip mouseout event handler
+              var tipMouseout = function(data) {
+                  tooltip.transition()
+                      .duration(300) // ms
+                      .style("opacity", 0); // don't care about position!
+              };
+//  dont add on events after transition   
+    
     // draw dots on the graph
     svg
       .selectAll(".dot")
@@ -105,7 +155,15 @@ $.getJSON(
     })
       .attr("cy", function(d) {
       return y(d.Place);
-    });
+    })
+    .on("mouseover", tipMouseover)
+    .on("mouseout", tipMouseout);
+    
+    
+    
+    
+    
+    
 
     //add text labels to the dots
     svg
@@ -126,5 +184,42 @@ $.getJSON(
     })
        .attr("font-size","12px")
       .attr("transform", "translate(17,+4)");
+
+    
+    svg
+.append("circle")
+      .attr("class", "dot")
+      .attr("r", 4)
+    .attr('cx',100)
+    .attr('cy')
+    
+    svg
+    .append('text')
+    
+    .text('No Doping allegations')
+   .attr('x',110)
+    .attr('y',5)
+    
+     svg
+.append("circle")
+      .attr("class", "dot")
+      .attr("r", 4)
+    .attr('fill','red')
+    .attr('cx',260)
+    .attr('cy')
+    
+    svg
+    .append('text')
+    
+    .text('Riders with Doping allegations')
+   .attr('x',270)
+    .attr('y',5)
+  
   }
+
+  
+  
+
+
+
 );
